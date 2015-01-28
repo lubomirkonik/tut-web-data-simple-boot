66490b2ce2275e05ef58cfd28d911331b2b11f6e
lubomirkonik <lubomirkonik@gmail.com>
2015-01-28 01:06:47 +0100

    persistance config

--- src/main/java/tut/webdata/config/PersistenceConfig.java
+++ src/main/java/tut/webdata/config/PersistenceConfig.java
@@ -1,12 +1,5 @@
 package tut.webdata.config;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
-
-import tut.webdata.domain.Order;
-import tut.webdata.domain.OrderStatus;
 import tut.webdata.repository.*;
 import tut.webdata.services.AccountService;
 import tut.webdata.services.MenuEventHandler;
@@ -15,8 +8,6 @@ import tut.webdata.services.OrderEventHandler;
 import tut.webdata.services.OrderService;
 //import tut.webdata.services.OrderStatusUpdateEventHandler;
 //import tut.webdata.services.OrderStatusUpdateService;
-
-
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
